217feb99339213d1548397da096b5856e27aa262
Klonochkin <di630538@yandex.ru>
2024-09-13 12:37:16 +0300

    feat: add backend

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, File, UploadFile,Form
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from typing import Annotated
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 import uvicorn
-from fastapi.responses import FileResponse
+from pymongo import MongoClient
+
+templates=Jinja2Templates(directory='templates')
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return FileResponse("index.html")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
+@app.get('/')
+async def welcome(request:Request) :
+    return templates.TemplateResponse(name='index.html',context={'request':request})
 
-
-@app.post("/send")
-def send():
-    content = "Hello, FastAPI!"
-
+@app.post('/send/')
+async def submit_form(
+    type_device: str = Form(...),
+    model_device: str = Form(...),
+    serial_number: str = Form(...),
+    ITAM_device: str = Form(...),
+    photo_device: str = Form(...),
+    photo_serial_number_device: str = Form(...),
+    photo_ITAM_device: str = Form(...)
+    ):
+    print(f"type_device: {type_device} ")
+    print(f"model_device: {model_device}")
+    print(f"serial_number: {serial_number}")
+    print(f"ITAM_device: {ITAM_device}")
+    print(f"photo_device: {photo_device}")
+    print(f"photo_serial_number_device: {photo_serial_number_device}")
+    print(f"photo_ITAM_device: {photo_ITAM_device}")
+    return {"message": type_device}
 
 
 
